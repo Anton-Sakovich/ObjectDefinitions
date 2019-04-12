2eeb43be88ba8099974d8b27ab40b07159b98a16
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-12 12:59:25 +0300

    Add project files and .gitignore

---
+++ ObjectDefinitions/ObjectDefinitions.m
@@ -0,0 +1,16 @@
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench Apr 12, 2019 *)
+
+BeginPackage["ObjectDefinitions`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
---
+++ PacletInfo.m
@@ -0,0 +1,11 @@
+(* Paclet Info File *)
+
+(* created 2019.04.12*)
+
+Paclet[
+  Name -> "ObjectDefinitions",
+  Version -> "0.0.1",
+  MathematicaVersion -> "6+",
+  Extensions -> {
+    {"Documentation", Language -> "English"}
+}]
