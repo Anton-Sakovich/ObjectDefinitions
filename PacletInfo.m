0cac7eec690c860051a2927fb08475e23f882faa
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-06-01 19:49:27 +0300

    Update PacletInfo

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,10 +1,10 @@
 (* Paclet Info File *)
 
-(* created 2019/05/31*)
+(* created 2019/06/01*)
 
 Paclet[
     Name -> "ObjectDefinitions",
-    Version -> "0.1.0",
+    Version -> "0.2.0",
     MathematicaVersion -> "10+",
     Description -> "Object oriented programing for Wolfram Language",
     Creator -> "Anton Sakovich",
