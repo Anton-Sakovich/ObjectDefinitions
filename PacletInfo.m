bd979f5088550988c292c7c32b15bac3d38e8fd1
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-09-06 18:02:35 +0300

    Increment version to 0.3.3

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,10 +1,10 @@
 (* Paclet Info File *)
 
-(* created 2019/07/16*)
+(* created 2019/09/06*)
 
 Paclet[
     Name -> "ObjectDefinitions",
-    Version -> "0.3.2",
+    Version -> "0.3.3",
     MathematicaVersion -> "10+",
     Description -> "Object oriented programing for Wolfram Language",
     Creator -> "Anton Sakovich",
